6c261d4978690b7eec6331c29ea13a6c3a806f5a
Josef Adamcik <josef.adamcik@gmail.com>
2020-06-20 19:32:04 +0200

    Fix key echo

--- publisher/publish.sh
+++ publisher/publish.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Save private ssh key from env var
-echo $JEKYLL_DEPLOY_KEY > /ssh.key
+echo "$JEKYLL_DEPLOY_KEY" > /ssh.key
 
 # Make sure the key has correct permissions
 chmod 0600 /ssh.key
 
 # Sync the files
-rsync \
+rsync -n \
   -e "ssh -i /ssh.key -o StrictHostKeyChecking=no -p $DEPLOY_PORT" \
   -rtzh \
   _site/ \
